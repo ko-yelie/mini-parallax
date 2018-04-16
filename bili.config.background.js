67c90984bbf0d195f258c41956c1ce690917838b
Ko.Yelie <ko.yelie@gmail.com>
2018-04-16 20:55:04 +0900

    Divide BackgroundParallax

---
+++ bili.config.background.js
@@ -0,0 +1,7 @@
+const defaultConfig = require('./bili.config.js')
+
+module.exports = Object.assign(defaultConfig, {
+  input: 'src/BackgroundParallax.js',
+  moduleName: 'BackgroundParallax',
+  name: 'background-parallax'
+})

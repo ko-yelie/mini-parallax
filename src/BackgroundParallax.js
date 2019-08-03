8cce5f13cdeb02f6d842ca4da3c60bcbc7492475
Ko.Yelie <ko.yelie@gmail.com>
2019-08-03 22:11:13 +0900

    Add destroy method

--- src/BackgroundParallax.js
+++ src/BackgroundParallax.js
@@ -1,9 +1,9 @@
-import ParallaxBase from './ParallaxBase'
+import { noop, getElements } from './modules/utility'
 
 /**
  * Automatically calculate the moving distance from the height of the parent element
  */
-export default class BackgroundParallax extends ParallaxBase {
+export default class BackgroundParallax {
   /**
    * @param {string|NodeList|Element|Element[]} target - Target elements (selector or element object)
    * @param {Object} [options={}]
@@ -12,8 +12,94 @@ export default class BackgroundParallax extends ParallaxBase {
    * @param {boolean} [options.isRound=false] - Whether transform style value is rounded or not
    * @param {boolean} [options.autoRun=true] - Whether to run automatically
    */
-  constructor (target, options) {
-    super(target, options)
+  constructor (target, options = {}) {
+    this._els = getElements(target)
+    if (this._els.length === 0) {
+      this._disabled = true
+      return
+    }
+
+    const {
+      onResize = noop,
+      onScroll = noop,
+      isRound = false,
+      autoRun = true
+    } = options
+
+    this._optionOnResize = onResize
+    this._optionOnScroll = onScroll
+    this._fTansform = `_getTransform${isRound ? 'Round' : ''}`
+    this._scrollTarget = document.scrollingElement || document.documentElement
+
+    this._onResize = () => {
+      this.update()
+      this._optionOnResize(this._windowHeight)
+    }
+    window.addEventListener('resize', this._onResize)
+
+    this._onLoad = () => {
+      this.update()
+    }
+    window.addEventListener('load', this._onLoad)
+
+    autoRun && this.run()
+  }
+
+  /**
+   * Cache various values
+   */
+  _cache () {
+    this._windowHeight = window.innerHeight
+    const scrollY = window.scrollY || window.pageYOffset
+
+    this._items = this._els
+      .map(el => {
+        el.style.transform = 'none'
+        const willChange = window.getComputedStyle(el).willChange
+        el.style.willChange = 'transform'
+        if (!(willChange === 'auto' || willChange === 'transform')) el.style.willChange += ', ' + willChange
+
+        return this._cacheElementPos(el, scrollY)
+      })
+      .filter(item => item)
+  }
+
+  /**
+   * Each frame of animation
+   */
+  _tick () {
+    const scrollTop = this._scrollTarget.scrollTop
+    if (scrollTop !== this._scrollTop) {
+      // When the scroll position changes
+      this._scrollTop = scrollTop
+      this._update()
+    }
+
+    this._animationFrameId = requestAnimationFrame(() => { this._tick() })
+  }
+
+  /**
+   * Update the position of each element
+   */
+  _update () {
+    this._items.forEach(item => this._updateElement(item))
+
+    this._optionOnScroll(this._scrollTop)
+  }
+
+  /**
+   * Return the value of transform
+   */
+  _getTransform (position) {
+    return `translate3d(0, ${position}px, 0)`
+  }
+
+  /**
+   * Return the value of transform
+   * In order to avoid problems such as bleeding, convert the number to an integers
+   */
+  _getTransformRound (position) {
+    return `translate3d(0, ${Math.round(position)}px, 0)`
   }
 
   /**
@@ -53,4 +139,51 @@ export default class BackgroundParallax extends ParallaxBase {
       }
     }
   }
+
+  /**
+   * Run animation
+   */
+  run () {
+    if (this._disabled) return
+
+    this._cache()
+    this._animationFrameId = requestAnimationFrame(() => { this._tick() })
+  }
+
+  /**
+   * Update cache and position
+   */
+  update () {
+    if (this._disabled) return
+
+    this._cache()
+    this._update()
+  }
+
+  /**
+   * Destroy instance
+   */
+  destroy () {
+    if (!this._els) return
+
+    cancelAnimationFrame(this._animationFrameId)
+
+    this._els.forEach(el => {
+      el.style.transform = null
+      el.style.willChange = null
+    })
+
+    window.removeEventListener('resize', this._onResize)
+    window.removeEventListener('load', this._onLoad)
+  }
 }
+
+/**
+ * @typedef {function} onResize
+ * @param {number} windowHeight - `window.innerHeight`
+ */
+
+/**
+ * @typedef {function} onScroll
+ * @param {number} scrollTop - `document.scrollingElement.scrollTop`
+ */
--- src/NormalParallax.js
+++ src/NormalParallax.js
@@ -1,10 +1,9 @@
-import { noop } from './lib/utility'
-import ParallaxBase from './ParallaxBase'
+import { noop, getElements } from './modules/utility'
 
 /**
  * Parallax library
  */
-export default class NormalParallax extends ParallaxBase {
+export default class NormalParallax {
   /**
    * @param {string|NodeList|Element|Element[]} target - Target elements (selector or element object)
    * @param {Object} [options={}]
@@ -17,19 +16,40 @@ export default class NormalParallax extends ParallaxBase {
    * @param {function} [options.isSP=noop] - Function to determine whether SP display or not
    */
   constructor (target, options = {}) {
+    this._els = getElements(target)
+    if (this._els.length === 0) {
+      this._disabled = true
+      return
+    }
+
     const {
       speed = 0.1,
       speedSp = speed,
       isSP = noop,
+      onResize = noop,
+      onScroll = noop,
+      isRound = false,
       autoRun = true
     } = options
 
-    options.autoRun = false
-    super(target, options)
-
+    this._optionOnResize = onResize
+    this._optionOnScroll = onScroll
+    this._fTansform = `_getTransform${isRound ? 'Round' : ''}`
+    this._scrollTarget = document.scrollingElement || document.documentElement
     this._speedPc = speed
     this._speedSp = speedSp
     this._isSP = isSP
+
+    this._onResize = () => {
+      this.update()
+      this._optionOnResize(this._windowHeight)
+    }
+    window.addEventListener('resize', this._onResize)
+
+    this._onLoad = () => {
+      this.update()
+    }
+    window.addEventListener('load', this._onLoad)
 
     autoRun && this.run()
   }
@@ -41,8 +61,59 @@ export default class NormalParallax extends ParallaxBase {
     this._isSpCurrent = this._isSP()
     this._speed = this._isSpCurrent ? this._speedSp : this._speedPc
     this._speed *= window.innerWidth / window.innerHeight
+    this._windowHeight = window.innerHeight
+    const scrollY = window.scrollY || window.pageYOffset
 
-    super._cache()
+    this._items = this._els
+      .map(el => {
+        el.style.transform = 'none'
+        const willChange = window.getComputedStyle(el).willChange
+        el.style.willChange = 'transform'
+        if (!(willChange === 'auto' || willChange === 'transform')) el.style.willChange += ', ' + willChange
+
+        return this._cacheElementPos(el, scrollY)
+      })
+      .filter(item => item)
+  }
+
+  /**
+   * Each frame of animation
+   */
+  _tick () {
+    const scrollTop = this._scrollTarget.scrollTop
+    if (scrollTop !== this._scrollTop) {
+      // When the scroll position changes
+      this._scrollTop = scrollTop
+      this._update()
+    }
+
+    this._animationFrameId = requestAnimationFrame(() => { this._tick() })
+  }
+
+  /**
+   * Update the position of each element
+   */
+  _update () {
+    this._centerViewport = this._scrollTop + this._windowHeight / 2
+
+    this._items.forEach(item => this._updateElement(item))
+
+    this._optionOnScroll(this._scrollTop)
+  }
+
+  /**
+   * Return the value of transform
+   */
+  _getTransform (position) {
+    return `translate3d(0, ${position}px, 0)`
+  }
+
+  /**
+   * Return the value of transform
+   * In order to avoid problems such as bleeding, convert the number to an integers
+   */
+  _getTransformRound (position) {
+    return `translate3d(0, ${Math.round(position)}px, 0)`
   }
 
   /**
@@ -66,15 +137,6 @@ export default class NormalParallax extends ParallaxBase {
   }
 
   /**
-   * Update the position of each element
-   */
-  _update () {
-    this._centerViewport = this._scrollTop + this._windowHeight / 2
-
-    super._update()
-  }
-
-  /**
    * Update the position of one element
    */
   _updateElement (item) {
@@ -87,4 +149,51 @@ export default class NormalParallax extends ParallaxBase {
       item.el.style.transform = this[this._fTansform](position)
     }
   }
+
+  /**
+   * Run animation
+   */
+  run () {
+    if (this._disabled) return
+
+    this._cache()
+    this._animationFrameId = requestAnimationFrame(() => { this._tick() })
+  }
+
+  /**
+   * Update cache and position
+   */
+  update () {
+    if (this._disabled) return
+
+    this._cache()
+    this._update()
+  }
+
+  /**
+   * Destroy instance
+   */
+  destroy () {
+    if (!this._els) return
+
+    cancelAnimationFrame(this._animationFrameId)
+
+    this._els.forEach(el => {
+      el.style.transform = null
+      el.style.willChange = null
+    })
+
+    window.removeEventListener('resize', this._onResize)
+    window.removeEventListener('load', this._onLoad)
+  }
 }
+
+/**
+ * @typedef {function} onResize
+ * @param {number} windowHeight - `window.innerHeight`
+ */
+
+/**
+ * @typedef {function} onScroll
+ * @param {number} scrollTop - `document.scrollingElement.scrollTop`
+ */
